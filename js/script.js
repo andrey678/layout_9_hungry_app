4845849911b6fae29db3ea0f36724236a2ebc7fb
andrey678 <drew6496@yandex.ru>
2022-02-15 17:00:07 +0300

    Main section completed

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const sectionMain = document.createElement('section');
 //Добавление стилей
 sectionMain.style.backgroundColor = "#1F98F0";
 sectionMain.style.paddingTop = "35px";
-sectionMain.style.paddingBottom = "35px";
+sectionMain.style.paddingBottom = "102px";
 
 //Вставка
 body.prepend(sectionMain);
@@ -136,7 +136,7 @@ navListItems.forEach(li => {
 });
 
 function addWhiteUnderscore(event) {
-    console.log(event.target.tagName);
+
     if (event.target.tagName == 'LI') {
         event.target.style.paddingBottom = "8px";
         event.target.style.borderBottomWidth = "2px";
@@ -155,3 +155,146 @@ function removeWhiteUnderscore(event) {
 }
 
 
+
+//Добавляем Content Inner
+//Создание
+const contentInner = document.createElement('div');
+//Добавление стилей
+contentInner.style.cssText = `
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+//Вставка
+header.after(contentInner);
+
+
+
+//Добавляем Text Content Inner
+//Создание
+const textContentInner = document.createElement('div');
+//Добавление стилей
+textContentInner.style.cssText = `
+    max-width: 500px;
+`;
+//Вставка
+contentInner.prepend(textContentInner);
+
+
+//Добавляем mainHeading
+//Создание
+const mainHeading = document.createElement('h1');
+//Наполнение
+mainHeading.textContent = `Your Favourite Food delivery Partner.`;
+//Добавление стилей
+mainHeading.style.cssText = `
+    font-family: WorkSans;
+    font-weight: bold;
+    font-size: 51px;
+    line-height: 130%;
+    color: #FFFFFF;
+    margin-bottom:30px;
+`;
+//Вставка
+textContentInner.prepend(mainHeading);
+
+
+//Добавляем Main Text
+//Создание
+const mainText = document.createElement('p');
+//Наполнение
+mainText.textContent = `Food Delivery is a thriving business and there are many opportunities for this Businesses as its Growing.`;
+//Добавление стилей
+mainText.style.cssText = `
+    font-family: Lato;
+    font-size: 20px;
+    line-height: 150%;
+    color: #FFFFFF;
+    margin-bottom: 50px;
+`;
+//Вставка
+mainHeading.after(mainText);
+
+//Добавляем Buttons Inner
+//Создание
+const buttonsInner = document.createElement('div');
+//Добавление стилей
+buttonsInner.style.cssText = `
+    max-width: 409px;
+    display: flex;
+    justify-content: space-between;
+`;
+//Вставка
+mainText.after(buttonsInner);
+
+
+//Добавляем googlePlayButton и appStoreButton
+//Создание
+const googlePlayButton = document.createElement('a');
+const appStoreButton = document.createElement('a');
+//Наполнение
+googlePlayButton.href = '#';
+googlePlayButton.innerHTML = `<img src='img/google_play.svg' alt='Google Play Download Button'>`;
+appStoreButton.href = '#';
+appStoreButton.innerHTML = `<img src='img/appstore.svg' alt='App Store Download Button'>`;
+//Добавление стилей
+//Вставка
+buttonsInner.prepend(googlePlayButton);
+googlePlayButton.after(appStoreButton);
+
+
+//Добавляем Images Inner
+//Создание
+const imagesInner = document.createElement('div');
+//Добавление стилей
+imagesInner.style.cssText = `
+    position:relative;
+`;
+//Вставка
+textContentInner.after(imagesInner);
+
+//Добавляем Bottom Left Arc
+//Создание
+const bottomLeftArc = document.createElement('img');
+// Наполнение
+bottomLeftArc.src = 'img/ellipse_bottomleft.svg';
+//Добавление стилей
+bottomLeftArc.style.cssText = `
+    position: absolute;
+    bottom: -62px;
+    left: -81px;
+    
+`;
+//Вставка
+imagesInner.prepend(bottomLeftArc);
+
+
+//Добавляем Central Image
+//Создание
+const centralImage = document.createElement('img');
+//Наполнение
+centralImage.src = 'img/appimage.png';
+//Добавление стилей
+//Вставка
+bottomLeftArc.after(centralImage);
+
+//Добавляем Top Right Arc
+//Создание
+const topRightArc = document.createElement('img');
+// Наполнение
+topRightArc.src = 'img/ellipse_topright.svg';
+//Добавление стилей
+topRightArc.style.cssText = `
+    position: absolute;
+    top:0;
+    right: -81px;
+`;
+//Вставка
+centralImage.after(topRightArc);
+
+
+//Добавляем Header Inner
+//Создание
+//Добавление стилей
+//Вставка
